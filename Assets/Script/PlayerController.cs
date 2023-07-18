9a5935519dc4e996aaae19ebfbc68debb4d3e752
JunHyung777 <jameskjh7531@gmail.com>
2023-07-19 04:00:44 +0900

    feat Player
    1. 이동 및 점프 코드 수정
    2. 근접 공격 구현
    3. 대쉬 구현 (대쉬 중 Enemy Layer에게 무적)

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -8,68 +8,130 @@ public class PlayerController : MonoBehaviour
 
     Rigidbody2D PlayerRigid;
     Animator animator;
-    public float speed = 3f;
-    public float jumpPower = 3f;
 
-    public bool isJump = false;
+    private float jumpForce = 500.0f;
+
+    private float defaultForce;
+    private float defaultmaxSpeed;
+
+    private float walkForce = 30.0f;
+    private float maxWalkSpeed = 3.0f;
+
+    private bool isdash;
+    public float defaultTime;
+    private float dashTime;
+
     public bool isGround = true;
-
-
-    Vector2 movement = new Vector2();
 
     BoxCollider2D col2D;
 
     void Start()
     {
-        PlayerRigid = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
-        col2D = GetComponent<BoxCollider2D>();
+        this.PlayerRigid = GetComponent<Rigidbody2D>();
+        this.animator = GetComponent<Animator>();
+        this.col2D = GetComponent<BoxCollider2D>();
+
+        defaultForce = walkForce;
+        defaultmaxSpeed = maxWalkSpeed;
     }
+
+    private float curTime;
+    public float coolTime = 0.5f;
+    public Transform pos;
+    public Vector2 boxSize;
 
     // Update is called once per frame
     void Update()
     {
+        if (curTime <= 0) // 공격
+        {
+            if (Input.GetKey(KeyCode.Z))
+            {
+                Collider2D[] collider2Ds = Physics2D.OverlapBoxAll(pos.position, boxSize, 0);
+                foreach(Collider2D collider in collider2Ds)
+                {
+                    if (collider.tag == "Monster")
+                    {
+                        Debug.Log("성공!");
+                        // collider.GetComponent<MonsterComponent>().TakeDamage(1.0f);
+                    }
+                }
+
+                animator.SetTrigger("atk");
+                curTime = coolTime;
+            }
+        }
+        else
+        {
+            curTime -= Time.deltaTime;
+        }
+
         RaycastHit2D raycastHit = Physics2D.BoxCast(col2D.bounds.center, col2D.bounds.size, 0f, Vector2.down, 0.02f, LayerMask.GetMask("Ground"));
         if (raycastHit.collider != null)
             isGround = true;
         else 
             isGround = false;
 
-        movement.x = Input.GetAxisRaw("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space) && isGround) // 점프
+        {
+            this.PlayerRigid.AddForce(transform.up * this.jumpForce);
+        }
 
-        if (Input.GetButtonDown("Jump") && isGround)
-            isJump = true;
+        float key = 0.0f; // 좌우 이동
+        if (Input.GetKey(KeyCode.RightArrow)) key = 1.0f;
+        if (Input.GetKey(KeyCode.LeftArrow)) key = -1.0f;
 
-    }
+        float speedx = Mathf.Abs(this.PlayerRigid.velocity.x); // 플레이어 속도
 
-    void Move()
-    {
- 
-        if (Mathf.Abs(movement.x) > 0)
+        if (speedx < defaultmaxSpeed) // 스피드 제한
+        {
+            this.PlayerRigid.AddForce(transform.right * key * this.defaultForce);
+        }
+
+        if ((key != 0.0f) && isGround) // 움직이는 방향에 맞춰 반전
+        {
+            transform.localScale = new Vector3(key, 1, 1);
+        }
+
+        if (key != 0.0f) // 걷기 애니메이션 설정
         {
             animator.SetBool("isWalk", true);
-            transform.localScale = new Vector3(movement.x, transform.localScale.y);
         }
         else
+        {
             animator.SetBool("isWalk", false);
+        }
 
-       PlayerRigid.velocity = new Vector2(speed * movement.x, PlayerRigid.velocity.y);
-    }
+        // 대쉬
+        if (Input.GetKeyDown(KeyCode.F) && isGround)
+        {
+            isdash = true;
+        }
 
-    void Jump()
+        if (!isdash)
+        {
+            this.gameObject.layer = 0;
+        }
+
+        if (dashTime <= 0)
+        {
+            if (isdash)
+            {
+                dashTime = defaultTime;
+                isdash = false;
+            }
+        }
+        else
+        {
+            dashTime -= Time.deltaTime;
+            this.gameObject.layer = 7;
+            transform.Translate(key * 0.125f, 0, 0);
+        }
+    }    
+
+    private void OnDrawGizmos()
     {
-        if (!isJump)
-            return;
-
-      PlayerRigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
-      isJump = false;
-          
-    }
-
-    private void FixedUpdate()
-    {
-        Move();
-        Jump();
-
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(pos.position, boxSize);
     }
 }
