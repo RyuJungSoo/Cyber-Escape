a0c949adbf475d637e418543c71163be1c3445d4
choiwoohyck <xxxxd1000@naver.com>
2023-08-17 14:54:11 +0900

    change bullet

--- Assets/Script/BulletGenerator.cs
+++ Assets/Script/BulletGenerator.cs
@@ -5,27 +5,31 @@ using UnityEngine;
 public class BulletGenerator : MonoBehaviour
 {
     public GameObject BulletPrefab;
-    float span = 1f;
+    float span = 3.0f;
     float delta = 0;
 
-    public bool isShoot = false;
+    GameObject Player;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.Player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
         this.delta += Time.deltaTime;
-        if (this.delta > this.span && isShoot)
+        if (this.delta > this.span)
         {
             this.delta = 0;
-            GameObject go = Instantiate(BulletPrefab);
-            int px = Random.Range(232, 250);
-            go.transform.position = new Vector3(px, 6, -1);      
+            if (this.Player.transform.position.x > 230f)
+            {
+                GameObject go = Instantiate(BulletPrefab);
+                GetComponent<AudioSource>().Play();
+                int px = Random.Range(232, 250);
+                go.transform.position = new Vector3(px, 6, -1);
+            }
         }
     }
 }
