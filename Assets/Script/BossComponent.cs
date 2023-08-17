a5ebcf0b8ac9be7c38216a5af34045fd2fd506a9
choiwoohyck <xxxxd1000@naver.com>
2023-08-17 12:41:19 +0900

    보스 돌진패턴시 점프 안할경우 발판 지나갈수있게 수정

--- Assets/Script/BossComponent.cs
+++ Assets/Script/BossComponent.cs
@@ -19,18 +19,19 @@ public class BossComponent : MonoBehaviour
 
 
     bool isJump = false;
+    bool isJumpReady = true;
     bool isRush = false;
     bool firstSetting = false;
     bool isReady = true;
 
     bool changeBeltDirection = false;
 
-    float jumpTimer = 0f;
     float rushTimer = 0f;
+    float jumpTimer = 0f;
     float attackTimer = 0f;
     float jumpAngle = 0f;
     float gravity = 0f;
-    
+
     public float rushSpeed = 5f;
     public float rushDamage = 5f;
 
@@ -46,10 +47,8 @@ public class BossComponent : MonoBehaviour
     Vector3 jumpStartPos;
     Vector3 targetPos;
 
-
     void Start()
     {
-
         animator = GetComponent<Animator>();
         orginPos = transform.position;
         player = GameObject.Find("Player");
@@ -74,7 +73,7 @@ public class BossComponent : MonoBehaviour
         if (attackTimer >= 4f)
         {
             attackPattern = UnityEngine.Random.Range(1, 3);
-            
+           
             attackTimer = 0;
             isReady = false;
         }
@@ -87,7 +86,7 @@ public class BossComponent : MonoBehaviour
                 direction = transform.position.x > player.transform.position.x ? -1 : 1;
                 rushObstacles.SetActive(true);
                 jumpStartPos = transform.position;
-                targetPos = player.transform.position ;
+                targetPos = player.transform.position;
                 firstSetting = true;
             }
             if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f)
@@ -102,17 +101,35 @@ public class BossComponent : MonoBehaviour
 
             if (!firstSetting)
             {
+
+
                 direction = transform.position.x > player.transform.position.x ? -1 : 1;
-                targetPos = new Vector3(player.transform.position.x,transform.position.y,transform.position.z);
+                targetPos = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
                 jumpAngle = Mathf.Rad2Deg * Mathf.Acos(targetPos.x);
                 jumpStartPos = transform.position;
+                float distance = Mathf.Abs(targetPos.x - jumpStartPos.x);
+
+                if (distance < 3)
+                {
+                    float normalizedJumpSpeed = Mathf.InverseLerp(0, 10, distance);
+                    jumpSpeed = Mathf.Lerp(1, 5, normalizedJumpSpeed);
+                }
+
+                else if (distance > 3 && distance < 8)
+                {
+                    jumpSpeed = 5;
+                }
+                else if (distance > 8)
+                {
+                    jumpSpeed = 8;
+                }
+
                 firstSetting = true;
             }
 
             if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f)
             {
                 animator.SetBool("isJump", true);
-                Debug.Log("¤¾¤¾");
                 isJump = true;
             }
 
@@ -160,30 +177,40 @@ public class BossComponent : MonoBehaviour
 
         if (isJump)
         {
-
             float x0 = jumpStartPos.x;
             float x1 = targetPos.x;
             float distance = x1 - x0;
+
             float nextX = Mathf.MoveTowards(transform.position.x, x1, jumpSpeed * Time.deltaTime);
             float baseY = Mathf.Lerp(jumpStartPos.y, targetPos.y, (nextX - x0) / distance);
             float arc = 4 * (nextX - x0) * (nextX - x1) / (-0.25f * distance * distance);
+
             Vector3 nextPosition = new Vector3(nextX, baseY + arc, transform.position.z);
 
-            transform.position = nextPosition;
+            if (!float.IsNaN(nextPosition.y))
+                transform.position = nextPosition;
+            else
+            {
+                isJump = false;
+                firstSetting = false;
+                jumpCnt++;
+                jumpTimer = 0;
+            }
 
             if (nextPosition == targetPos)
             {
                 isJump = false;
                 firstSetting = false;
                 jumpCnt++;
+                jumpTimer = 0;
+            }
 
-                if (jumpCnt == 3)
-                {
-                    attackPattern = 0;
-                    isReady = true;
-                    changeBeltDirection = true;
-                    jumpCnt = 0;
-                }    
+            if (jumpCnt == 3)
+            {
+                attackPattern = 0;
+                isReady = true;
+                changeBeltDirection = true;
+                jumpCnt = 0;
             }
         }
     }
@@ -192,10 +219,18 @@ public class BossComponent : MonoBehaviour
     {
         if (collision.CompareTag("Player"))
         {
-            if (collision.gameObject.layer != 8)
+            if (collision.gameObject.layer != 8 && collision.GetComponent<PlayerController>().Hp > 0)
                 GameManager.Instance.PlayerDamage(rushDamage, true);
         }
     }
 
-  
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (collision.gameObject.layer != 8 && collision.GetComponent<PlayerController>().Hp > 0)
+                GameManager.Instance.PlayerDamage(rushDamage, true);
+        }
+    }
+
 }
