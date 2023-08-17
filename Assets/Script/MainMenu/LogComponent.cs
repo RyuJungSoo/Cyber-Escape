542fdd996818ca2b714e2d2989cd8236987521f1
JungSoo Ryu <goldfox10@naver.com>
2023-08-17 13:43:38 +0900

    add LogTyping Effect & Tutorial BGM

--- Assets/Script/MainMenu/LogComponent.cs
+++ Assets/Script/MainMenu/LogComponent.cs
@@ -10,28 +10,34 @@ public class LogComponent : MonoBehaviour
     private bool isFadeEnd;
     private int index = 0;
     public GameObject[] Logs;
+    public GameObject ClickLog;
+    public string[] Texts;
+    private bool isTyping;
+    private AudioSource audioSource;
 
     private void Awake()
     {
-        StartCoroutine("FadeIn");
+
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+        StartCoroutine("FadeIn");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isFadeEnd)
+        if (isFadeEnd && isTyping == false)
         {
             if (Input.GetMouseButtonDown(0))
             {
                 if (index < Logs.Length)
                 {
                     Logs[index].SetActive(true);
+                    StartCoroutine(Typing(index));
                     index++;
                 }
                 else
@@ -56,14 +62,29 @@ public class LogComponent : MonoBehaviour
             Image.color = new Color(Image.color.r, Image.color.g, Image.color.b, Image.color.a + Time.deltaTime);
             yield return null;
         }
-        Logs[index].SetActive(true);
-        index++;
+        ClickLog.SetActive(true);
         isFadeEnd = true;
+    }
+
+    IEnumerator Typing(int index)
+    {
+        audioSource.Play();
+        isTyping = true;
+        for (int i = 0; i <= Texts[index].Length; i++)
+        {
+            
+            //Debug.Log(Texts[index].Substring(0, i));
+            Logs[index].GetComponent<TMP_Text>().text = Texts[index].Substring(0, i);
+            yield return new WaitForSeconds(0.1f);
+            
+        }
+        isTyping = false;
+        audioSource.Stop();
     }
 
     IEnumerator TextFadeOut()
     {
-        
+        ClickLog.SetActive(false);
         while (Logs[Logs.Length-1].GetComponent<TMP_Text>().color.a > 0)
         {
             //Debug.Log("Start");
